ac206bb1029ae9b7dac1216228753578214c9baa
Antonioflo13 <floreantonio@hotmail.it>
2022-06-13 18:50:19 +0200

    add google verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="6T0AqQKpOi4xxo_0IIShK5uhWgY2_UrPPSXHLb4oXLM"
+        />
         <meta name="keywords" content="puglia, casa vacanze, apuglia, Ostuni" />
         <meta name="author" content="AF" />
         <link rel="shortcut icon" type="image/x-icon" href="redHouse.ico" />
