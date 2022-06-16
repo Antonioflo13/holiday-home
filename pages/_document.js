ae9eef9d365e2d617a7f1f3089bd909c7f7832ad
Antonioflo13 <floreantonio@hotmail.it>
2022-06-16 13:53:45 +0200

    fix .env

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ export default function Document() {
         <link rel="shortcut icon" type="image/x-icon" href="redHouse.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300&display=swap" rel="stylesheet"/>
       </Head>
       <body>
         <Main />
